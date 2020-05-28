62945cf84d04316d6206ac4b89fd311dbade7230
Sara <saracorsob@gmail.com>
2020-05-28 16:39:42 +0200

    Aggiunta validazione + correzione jsn 3

--- js/script.js
+++ js/script.js
@@ -29,67 +29,74 @@
 // document.getElementById('ciao').innerHTML = messaggio;
 
 
-// JSNACKS-2
-//// L’utente inserisce due parole in successione, con due prompt. Il software
-//// stampa prima la parola più corta, poi la parola più lunga.
-
-// Utente inserisce due parole
-// Prima parola
-var primaParola = prompt("Inserisci la prima parola.");
-
-while ( primaParola.length == 0 || !(isNaN(primaParola)) ) {
-  primaParola = prompt("La parola inserita non è valida. Inserisci la prima parola.");
-}
-// Seconda Parola
-var secondaParola = prompt("Inserisci la seconda parola.");
-
-while ( secondaParola.length == 0 || !(isNaN(secondaParola)) ) {
-  secondaParola = prompt("La parola inserita non è valida. Inserisci la prima parola.");
-}
-
-//Variabile del messaggio finale
-var messaggio = 'Le parole sono uguali';
-
-// Calcolo e paragono la lunghezza delle parole
-if ( primaParola.length > secondaParola.length ) {
-  messaggio = secondaParola + '-' + primaParola;
-} else if ( secondaParola.length > primaParola.length ) {
-  messaggio = primaParola  + '-' + secondaParola;;
-}
-
-// Stampo le parole in base alla lunghezza
-document.getElementById('ciao').innerHTML = messaggio;
-
-
-// // JSNACKS-3
-// //// Il software deve chiedere per 5 volte all’utente di inserire un numero.
-// //// Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma
-// //// in due versioni, con il for e con il while.
+// // JSNACKS-2
+// //// L’utente inserisce due parole in successione, con due prompt. Il software
+// //// stampa prima la parola più corta, poi la parola più lunga.
 //
-// var numeri = [];
-// // Chiedo un numero all'utente per 5 volte
+// // Utente inserisce due parole
+// // Prima parola
+// var primaParola = prompt("Inserisci la prima parola.");
+//
+// while ( primaParola.length == 0 || !(isNaN(primaParola)) ) {
+//   primaParola = prompt("La parola inserita non è valida. Inserisci la prima parola.");
+// }
+// // Seconda Parola
+// var secondaParola = prompt("Inserisci la seconda parola.");
+//
+// while ( secondaParola.length == 0 || !(isNaN(secondaParola)) ) {
+//   secondaParola = prompt("La parola inserita non è valida. Inserisci la prima parola.");
+// }
+//
+// //Variabile del messaggio finale
+// var messaggio = 'Le parole sono uguali';
+//
+// // Calcolo e paragono la lunghezza delle parole
+// if ( primaParola.length > secondaParola.length ) {
+//   messaggio = secondaParola + '-' + primaParola;
+// } else if ( secondaParola.length > primaParola.length ) {
+//   messaggio = primaParola  + '-' + secondaParola;;
+// }
+//
+// // Stampo le parole in base alla lunghezza
+// document.getElementById('ciao').innerHTML = messaggio;
+
+
+// JSNACKS-3
+//// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//// Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma
+//// in due versioni, con il for e con il while.
+
+var numeri = [];
+// Chiedo un numero all'utente per 5 volte
+
 // // WHILE
 // while (numeri.length < 5) {
 //   var numero = parseInt(prompt('Inserisci un numero.'));
+//   if ( isNaN(numero) || numero == '' ) {
+//     numero = parseInt(prompt('Il numero inserito non è valido. Inserisci un numero.'));
+//   }
 //   numeri.push(numero);
 // };
 // console.log(numeri);
-//
-// // FOR
-// // for (var i = 0; i < 5; i++) {
-// //   var numero = parseInt(prompt('Inserisci un numero.'));
-// //   numeri.push(numero);
-// // };
-// // console.log(numeri);
-//
-// // Sommo i numeri e stampo il risultato
-// var somma = 0;
-// for (var i = 0; i < numeri.length; i++) {
-//   somma = somma + numeri[i];
-// };
-//
-// document.getElementById('ciao').innerHTML = somma;
-//
+
+// FOR
+for (var i = 0; i < 5; i++) {
+  var numero = parseInt(prompt('Inserisci un numero.'));
+  while ( isNaN(numero) || numero == '' ) {
+    numero = parseInt(prompt('Il numero inserito non è valido. Inserisci un numero.'));
+  }
+  numeri.push(numero);
+};
+
+// Sommo i numeri e stampo il risultato
+var somma = 0;
+for (var i = 0; i < numeri.length; i++) {
+  somma = somma + numeri[i];
+};
+
+document.getElementById('ciao').innerHTML = 'La somma finale è: ' + somma;
+
+
 // // JSNACKS-4
 // //// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 // //// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
@@ -118,7 +125,8 @@ document.getElementById('ciao').innerHTML = messaggio;
 // } else {
 //   alert('Mi dispiace non puoi partecipare.')
 // }
-//
+
+
 // // JSNACKS-5
 // //// Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre
 // //// che compongono il numero.
